91e313fff13f7258b8ed1dc780f3635eab8dfdc4
Astralcircle <142503363+Astralcircle@users.noreply.github.com>
2025-08-11 22:47:45 +0300

    Change content addon

--- lua/autorun/mediaplayer.lua
+++ lua/autorun/mediaplayer.lua
@@ -54,7 +54,7 @@ local function LoadMediaPlayer()
 
 	if SERVER then
 		-- Add mediaplayer models
-		resource.AddWorkshop( "546392647" )
+		resource.AddWorkshop( "3547065959" )
 
 		-- download clientside includes
 		AddCSLuaFile "includes/modules/browserpool.lua"
