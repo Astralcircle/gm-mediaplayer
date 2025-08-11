090ac8ac47413caed04546b58034732b5ccdf7a3
Astralcircle <142503363+Astralcircle@users.noreply.github.com>
2025-08-11 06:09:12 +0300

    Remove useless log system

--- lua/mediaplayer/init.lua
+++ lua/mediaplayer/init.lua
@@ -21,7 +21,6 @@ AddCSLuaFile "shared.lua"
 AddCSLuaFile "sh_events.lua"
 AddCSLuaFile "sh_mediaplayer.lua"
 AddCSLuaFile "sh_services.lua"
-AddCSLuaFile "sh_history.lua"
 AddCSLuaFile "sh_metadata.lua"
 AddCSLuaFile "sh_cvars.lua"
 
